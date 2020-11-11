d117f2b84314c27bcdcf399bf192797e13d714bd
jiten228 <jiten228@yahoo.com>
2020-11-11 17:02:23 -0600

    End of Target

--- src/LogoutUtility.java
+++ src/LogoutUtility.java
@@ -2,5 +2,6 @@ public class LogoutUtility {
     public static void main(String[] args) {
         // Add some awesome notes
         System.out.println("End of task");
+        System.out.println("Target finished");
     }
 }
